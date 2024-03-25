6eec1eee2f5fae597b18f34684af3c5317935197
joeywhite1 <113400338+joeywhite1@users.noreply.github.com>
2024-03-24 22:10:59 -0400

    Update syscalls.c

--- part3/syscalls.c
+++ part3/syscalls.c
@@ -1,1 +1,38 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
 
+// sys calls stubs
+int (*STUB_start_elevator)(void) = 548;
+int (*STUB_issue_request)(int, int, int) = 549;
+int (*STUB_stop_elevator)(void) = 550;
+EXPORT_SYMBOL(STUB_start_elevator);
+EXPORT_SYMBOL(STUB_issue_request);
+EXPORT_SYMBOL(STUB_stop_elevator);
+
+
+// sys call wrappers
+SYSCALL_DEFINE0(start_elevator) {
+	printk(KERN_NOTICE "Inside start_elevator block. %s", __FUNCTION__);
+	if (STUB_start_elevator != 548)
+		return STUB_start_elevator();
+	else
+		return -ENOSYS;
+}
+
+SYSCALL_DEFINE3(issue_request, int, start_floor, int, destination_floor, int, type) {
+	printk(KERN_NOTICE "Inside issue_request block. %s: The int are %d, %d, %d", __FUNCTION__, start_floor, destination_floor, type);
+        if (STUB_issue_request != 549)
+                return STUB_issue_request(start_floor, destination_floor, type);
+        else
+                return -ENOSYS;
+}
+
+SYSCALL_DEFINE0(stop_elevator) {
+        printk(KERN_NOTICE "Inside stop_elevator block. %s", __FUNCTION__);
+        if (STUB_stop_elevator != 550)
+                return STUB_stop_elevator();
+        else
+                return -ENOSYS;
+}
